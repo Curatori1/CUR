b4c4d82f21e215aaf2524abeec08a69f96954c41
Curatori D'Immagini <gennymottola88@gmail.com>
2026-02-13 13:42:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const ISSUER = "GD22GBKOUYWC3C2QDZPMT27GBD2L6STAPXN2QEZB3HX4GNISW3P7XSQK";
+const ISSUER_ID = "GD22GBKOUYWC3C2QDZPMT27GBD2L6STAPXN2QEZB3HX4GNISW3P7XSQK";
 
 function buy(assetCode) {
-    console.log("Ordine avviato per:", assetCode);
-    alert("🌟 ECCELLENZA CURATORI1 🌟\n\nHai selezionato il servizio: " + assetCode + "\nConnessione alla Blockchain Stellar in corso...");
+    console.log("Servizio selezionato: " + assetCode);
+    alert("CURATORI1 ATELIER\n\nHai selezionato il servizio di lusso: " + assetCode + "\nConnessione al Ledger Stellar in corso...");
 }
